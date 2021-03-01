3adf77c21fcc91d68809656a6fd6c4254db36251
Alex Nordin <apnordin@gmail.com>
2021-03-01 15:56:32 -0500

    Correcting again for heroku build

---
+++ client/build/precache-manifest.fb6d5f042220f24d1be3ad7049214d42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ccc49a919553050f5c5",
+    "url": "/static/css/main.2c09a614.chunk.css"
+  },
+  {
+    "revision": "1ccc49a919553050f5c5",
+    "url": "/static/js/main.52b87cc8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b31982e59926a129a1ef",
+    "url": "/static/css/2.de6e3290.chunk.css"
+  },
+  {
+    "revision": "b31982e59926a129a1ef",
+    "url": "/static/js/2.24e8dc91.chunk.js"
+  },
+  {
+    "revision": "0e78d8c02b7ff31763eef34e9ef364bd",
+    "url": "/index.html"
+  }
+];
