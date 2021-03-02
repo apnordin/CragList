45ef12656df4c3cee69b725d318c70dcfacf1653
Alex Nordin <apnordin@gmail.com>
2021-03-02 12:13:34 -0500

    Added icons, formatted login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import AUTH from "../utils/AUTH";
 import Signup from "../components/Signup"
 import PropTypes from 'prop-types';
+import logo from "../assets/logowhite.png";
 
 export default function Login ({ setToken }) {
 
@@ -28,49 +29,71 @@ export default function Login ({ setToken }) {
     }
 
     return (
-        <div className="container">
-            <div className="card login-card">
-                <div className="card-header">
-                    <h3 className="welcomeh3">Welcome to CragList!</h3>
-                </div>
-                <div className="card-body">
-                    <div className="mt-2 mb-4 px-5 text-center">
-                        <h3 className="mb-3">Existing Users</h3>
-                        <form onSubmit={handleSubmit}>
-                            <div className="row form-group">
-                                <div className="col" size="12">
-                                <input
-                                    className="form-control loginsignup"
-                                    type="text"
-                                    placeholder="Username"
-                                    name="username"
-                                    onChange={e => setUsername(e.target.value)}
-                                    />
-                                </div>
-                            </div>
-                            <div className="row form-group">
-                                <div className="col" size="12">
-                                <input
-                                    className="form-control loginsignup"
-                                    type="password"
-                                    placeholder="Password"
-                                    name="password"
-                                    onChange={e => setPassword(e.target.value)}
-                                    />
-                                </div>
-                            </div>
-                            <button className="btn loginsignupbtn" type="submit">
-                                Log In
-                            </button>
-                        </form>
-                        <div className="line-break">
-                            <hr className="mt-4 mb-4"></hr>
+        // <div className="container">
+        <div className="yosemite allpage">
+            <div className="row allpage">
+                <div className="col-3">
+                    <div className="login-card">
+                        <div className="card-header round-0">
+                            <span className="welcomeh3">Welcome to CragList!
+                            </span>
+                            <img className="logo float-right" alt= "logo" src ={logo}>
+                            </img>
                         </div>
+                        <div className="card-body">
+                            <div className="mt-2 mb-4 px-5 text-center">
+                                <h3 className="mb-3">Existing Users</h3>
+                                <form onSubmit={handleSubmit}>
+                                    <div className="row form-group">
+                                        <div className="col" size="12">
+                                        <input
+                                            className="form-control loginsignup"
+                                            type="text"
+                                            placeholder="Username"
+                                            name="username"
+                                            onChange={e => setUsername(e.target.value)}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="row form-group">
+                                        <div className="col" size="12">
+                                        <input
+                                            className="form-control loginsignup"
+                                            type="password"
+                                            placeholder="Password"
+                                            name="password"
+                                            onChange={e => setPassword(e.target.value)}
+                                            />
+                                        </div>
+                                    </div>
+                                    <button className="btn loginsignupbtn" type="submit">
+                                        Log In
+                                    </button>
+                                </form>
+                                <div className="line-break">
+                                    <hr className="mt-4 mb-4"></hr>
+                                </div>
+                            </div>
+                            <Signup setToken={setToken} />
+                        </div>
+                    
                     </div>
-                    <Signup setToken={setToken} />
+                </div>
+                <div className="col-9">
+                    <div className="row">
+                    <div className="welcomeblurb">
+                        <h3 className="welcomeblurbh3 top">
+                            Welcome to CragList, a website for climbers to find for climbing routes anywhere in the world.
+                        </h3>
+                        <h3 className="welcomeblurbh3">
+                            Add your own routes, search or browse routes added by other users, post on your favorite route's feed, and chat live with other users.
+                        </h3>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
+        // </div>
     )
 }
 
